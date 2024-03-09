82497a6193aeabc20246d8a962cd05288a2cdfad
Kamedin <dev@dev.dev>
2024-03-09 14:49:35 +0100

    added bootstrap, created form component including file upload

---
+++ src/components/TheUpload.vue
@@ -0,0 +1,6 @@
+<template>
+    <form class="container d-flex flex-column justify-content-start align-items-center">
+        <h2>Upload your .csv</h2>
+        <input type="file" id="fileupload" name="fileupload">
+    </form>
+</template>
